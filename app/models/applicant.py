85ab2202ba5025ded326f982cf6480e5fa9b38e0
nimirzuher-svg <301150864+nimirzuher-svg@users.noreply.github.com>
2026-07-16 00:18:24 +0300

    feat(model): create applicant model

---
+++ app/models/applicant.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from app.database.base import Base
+
+
+class Applicant(Base):
+    __tablename__ = "applicants"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    phone = Column(String)
+    resume_path = Column(String)
+    job_id = Column(Integer, ForeignKey("jobs.id"))
